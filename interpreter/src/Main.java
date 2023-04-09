2ebb26fb20c9531919dd1d3d83ebc06456b14f6e
Neversett1e <stepanenkodanila@yandex.ru>
2023-04-09 17:58:36 +0300

    refresh

--- interpreter/src/Main.java
+++ interpreter/src/Main.java
@@ -1,38 +1,44 @@
-public class Main {
-
-    /**
-     * Алгоритм бинарного поиска для возврата позиции ключа `x` в подмассив A[left…right]
-     *
-     * @param A     массив, в котором происходит поиск
-     * @param left  левая граница пространства поиска
-     * @param right правая граница пространства поиска
-     * @param x     искомый ключ
-     * @return позицию ключа `x` в массиве `A` или `-1`, если элемент не найден
-     */
-    private static int binarySearch(int[] A, int left, int right, int x) {
+class Main
+{
+    // Алгоритм бинарного поиска для возврата позиции ключа `x` в
+    // подмассив A[left…right]
+    private static int binarySearch(int[] A, int left, int right, int x)
+    {
+        // базовое состояние (пространство поиска исчерпано)
         if (left > right) {
             return -1;
         }
 
-        int mid = left + (right - left) / 2;
+        // находим среднее значение в пространстве поиска и
+        // сравнивает его с ключом
 
+        int mid = (left + right) / 2;
+
+        // может произойти переполнение. Использовать
+        // int mid = left + (right - left) / 2;
+
+        // базовое условие (ключ найден)
         if (x == A[mid]) {
             return mid;
-        } else if (x < A[mid]) {
+        }
+
+        // отбросить все элементы в правильном пространстве поиска,
+        // включая средний элемент
+        else if (x < A[mid]) {
             return binarySearch(A, left, mid - 1, x);
-        } else {
+        }
+
+        // отбрасываем все элементы в левой области поиска,
+        // включая средний элемент
+        else {
             return binarySearch(A, mid + 1, right, x);
         }
     }
 
-    /**
-     * Алгоритм экспоненциального поиска
-     *
-     * @param A массив, в котором происходит поиск
-     * @param x искомый ключ
-     * @return позицию ключа `x` в массиве `A` или `-1`, если элемент не найден
-     */
-    public static int exponentialSearch(int[] A, int x) {
+    // Возвращает позицию ключа `x` в заданном массиве `A` длины `n`
+    public static int exponentialSearch(int[] A, int x)
+    {
+        // базовый вариант
         if (A == null || A.length == 0) {
             return -1;
         }
@@ -41,17 +47,16 @@ public class Main {
 
         // найти диапазон, в котором будет находиться ключ `x`
         while (bound < A.length && A[bound] < x) {
-            bound *= 2;
+            bound *= 2;        // вычисляем следующую степень числа 2
         }
 
         // вызов бинарного поиска на A[bound/2 … min(bound, n-1)]
-        return binarySearch(A, bound / 2, Math.min(bound, A.length - 1), x);
+        return binarySearch(A, bound/2, Integer.min(bound, A.length - 1), x);
     }
 
-    /**
-     * Точка входа в программу
-     */
-    public static void main(String[] args) {
+    // Алгоритм экспоненциального поиска
+    public static void main(String[] args)
+    {
         int[] A = {2, 5, 6, 8, 9, 10};
         int key = 9;
 
@@ -59,7 +64,8 @@ public class Main {
 
         if (index != -1) {
             System.out.println("Element found at index " + index);
-        } else {
+        }
+        else {
             System.out.println("Element not found in the array");
         }
     }
